b0dbcf1014d5cb1a1876ddaf694c3f7365d69626
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-02-08 01:10:41 +0000

    Atualizar

--- html/play/play.js
+++ html/play/play.js
@@ -69,7 +69,7 @@ function buscarBoardgame(rodada_id){
 
 function getBoardgame(rodada_id){
   let tmp_boardgame = sessionStorage.boardgamesToday;
-  if(!tmp_boardgame === undefined || !tmp_boardgame === "undefined"){
+  if(!(tmp_boardgame === undefined) || !(tmp_boardgame === "undefined")){
     boardgamesToday.forEach(boardgame => {
       if(boardgame.dados.boardgame_id == rodada_id){
         setBoardGame(boardgame);
