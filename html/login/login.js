2ecf4c3ccc7f07ae6e247f6f4c33aa252bc252f0
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-02-08 20:58:57 +0000

    Atualizar

--- html/login/login.js
+++ html/login/login.js
@@ -1,14 +1,16 @@
+firebase.auth().onAuthStateChanged( (user) => {
+    if (user) {
+        window.location.href = "../home/confirmacao.html";
+    }
+})
+
 function login() {
     //var user_UID;
     firebase.auth().signInWithEmailAndPassword(
         form.email().value, form.password().value
     ).then((userCredential) => {
-        //user_UID = userCredential.user.auth.currentUser.uid;
         user = userCredential.user.auth.currentUser;
         sessionStorage.setItem("userUid", user_UID);
-        window.location.href = "../home/confirmacao.html";
-        console.log("Usuário logou:" + userCredential.user.uid);
-        //getCurrentUser(user_UID)
     }).catch(error => {
         console.log(getErrorMessage(error));
     });
