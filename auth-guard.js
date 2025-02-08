0c60ff816b23cd0b74c2f62777fe865a8c0e296a
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-02-08 20:45:06 +0000

    Atualizar

--- auth-guard.js
+++ auth-guard.js
@@ -1,9 +1,6 @@
 firebase.auth().onAuthStateChanged( (user) => {
-    if (user) {
-        //sessionStorage.setItem("userUid", user.uid);
-        window.location.href = "../home/home.html";
-    }else{
-        //sessionStorage.clear;
+    if (!user) {
+        sessionStorage.clear;
         window.location.href = "../login/login.html";
     }
 })
