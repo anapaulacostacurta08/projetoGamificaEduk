bf35ee144e5b7692dd0de9c37f25447987601bdc
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-03-03 21:48:15 +0000

    Atualizar

--- html/event/event-dashboard.js
+++ html/event/event-dashboard.js
@@ -3,16 +3,17 @@ firebase.auth().onAuthStateChanged((User) => {
     let events_list = document.getElementById("events_list");
     eventService.getEventsByUserUID(User.uid).then((events) => {
         events.forEach(event => {
-            events_list.innerHTML = `<div class="card"><span>${event.dados.name} - ${event.dados.state}</span>`;
+            let card_event = `<div class="card"><span>${event.dados.name} - ${event.dados.state}</span>`;
             let players = event.dados.players;
             players.forEach(player => {
                 if(player.user_UID === User.uid){
-                  events_list.innerHTML =   events_list.innerHTML + 
+                  card_event =   card_event + 
                     `<span id="coin" class="col-sm-3 ml-auto"><span class="badge rounded-pill bg-success"><span id="coins" class="badge bg-light text-dark">${player.coins}</span>&nbsp;AB@ COINS</span><br/></span>`;
                 }
             })
-            events_list.innerHTML =   events_list.innerHTML + `</div>`;
+            card_event =   card_event + `</div>`;
         })
+        events_list.innerHTML = card_event;
     })
   } 
 });
