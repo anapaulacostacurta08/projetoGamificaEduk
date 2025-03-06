ec7b36f07b5cec068d0e74b811c90b44ef66982d
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-03-06 18:04:07 +0000

    Atualizar

--- html/event/activities-event-dashboard.js
+++ html/event/activities-event-dashboard.js
@@ -2,90 +2,111 @@ firebase.auth().onAuthStateChanged((User) => {
   if (User) {
     var players;
     var player;
-    let linhas = ''; 
-    const my_activities = document.getElementById("my_activities");
+    let active_activities_list = document.getElementById("ctive_activities_list");
+    let closed_activities_list = document.getElementById("closed_activities_list");
     const params = new URLSearchParams(window.location.search);
     var event_uid = params.get('event_uid');
+    var event_state = params.get('state');
 
-    activityService.getActivitybyEventID(event_uid).then((activities) => {
-      activities.forEach(activity => {
-        players = activity.dados.players;
-        player = players.find(player => player.user_UID == User.uid);                      
-        var activity_uid = activity.uid;
-        var activity_dados = activity.dados;
-        let entrar;
-        if (activity_dados.state =="started"){
-          entrar = `<td><span><input type="radio" id="activity_uid" class="activity_uid" value="${activity_uid}">${activity_dados.id}</span></td>`;
-        }else{
-          entrar = `<td><span>&nbsp;&nbsp;&nbsp${activity_dados.id}</span></td>`;
+    if (event_state === "started"){
+      activityService.getActivitybyEventID(event_uid).then((activities) => {
+        let card_active_activity = ``;
+        let card_closed_activity = ``;
+        activities.forEach(activity => {
+          players = activity.dados.players;
+          let card_activity = `<span class="activity_dados" id="${activity.uid}">${activity.dados.name}</span>`;
+          player = players.find(player => player.user_UID == User.uid); 
+          let date_start = `<span id="date_start">${activity.dados.date_start}</span>-<span id="time_start">${activity.dados.time_start}</span>`;
+          let date_final = `<span id="date_final">${activity.dados.date_final}</span>-<span id="time_final">${activity.dados.time_final}</span>`;
+          let card_points = `<span id="ponts" class="col-sm-4">`+
+            `<span class="badge rounded-pill bg-primary border border-2 border-dark p-1 m-1">`+
+                `<span id="level" class="badge bg-light text-dark border border-2 border-dark"></span>&nbsp;&nbsp;NÍVEL&nbsp;`+
+            `</span>`+
+            `<span class="badge rounded-pill bg-info border border-2 border-dark p-1 m-1">`+
+                `<span id="score" class="badge bg-light text-dark border border-2 border-dark"></span>&nbsp;PONTOS NÍVEL`+
+            `</span>`+
+          `</span>`;
+
+          if(activity.dados.state === "started"){
+            card_active_activity = card_active_activity +`<div class="card_active">${card_activity}${date_start}${date_final}${card_points}</div>`;
+          }
+          if (activity.dados.state === "finished"){
+            card_closed_activity = card_closed_activity +`<div class="card_closed>${card_activity}${date_start}${date_final}${card_points}</div>`;
+          }
+        })
+        active_activities_list.innerHTML = card_active_activity;
+        closed_activities_list.innerHTML = card_closed_activity;
+        const card_active = events_list.querySelectorAll(".card_active");
+        const card_closed = events_list.querySelectorAll(".card_closed");
+
+        // set onclick attribute to all available cards active
+        for (i = 0; i < card_active.length; i++) {
+          card_active[i].setAttribute("onclick", "cardActiveSelected(this)");
         }
-        let date = '<td><span>'+activity_dados.date_start+'</span>-<span>'+activity_dados.time_start+'</span>-<span>'+activity_dados.date_final+'</span>-<span>'+activity_dados.time_final+'</span></td>';
-        let state = '<td><span>'+activity_dados.state+'</span></td>';
-        linhas = linhas + '<tr>'+entrar+date+state+'</tr>';
+          // set onclick attribute to all available cards closed
+        for (i = 0; i < card_closed.length; i++) {
+          card_closed[i].setAttribute("onclick", "cardClosedSelected(this)");
+        }
       })
-      let tbody = '<tbody>'+linhas+'</tbody>';
-      let thead = '<thead><tr><th>Atividade</th><th>Período</th><th>Status</th></tr></thead>';     
-      let table = '<table class="table table-hover" align="center">'+ thead + tbody+'</table>';
-      my_activities.innerHTML = table;
-    })
 
-    document.getElementById("play-form").addEventListener("submit", function(event) {
-      event.preventDefault();
-      // Captura os dados do formulário
-      let uid = my_activities.querySelector(".activity_uid").value;
-      let score = 0;
-      let user_UID = User.uid;
-      let ckeckin_date = (new Date()).toLocaleDateString('pt-BR');
-      let ckeckin_time = (new Date()).toLocaleTimeString('pt-BR');
-      let timestamp = new Date().getTime();
+      document.getElementById("play-form").addEventListener("submit", function(event) {
+        event.preventDefault();
+        // Captura os dados do formulário
+        let uid = my_activities.querySelector(".activity_uid").value;
+        let score = 0;
+        let user_UID = User.uid;
+        let ckeckin_date = (new Date()).toLocaleDateString('pt-BR');
+        let ckeckin_time = (new Date()).toLocaleTimeString('pt-BR');
+        let timestamp = new Date().getTime();
 
-      activityService.getActivitybyUid(uid).then((activity) => {
-              let data_start = event.dados.date_start.split("/");
-              let time_start = event.dados.time_start.split(":");
-              let data_time_start = new Date(data_start[2],data_start[1]-1,data_start[0],time_start[0],time_start[1]);
-              let data_final = event.dados.date_final.split("/");
-              let time_final = event.dados.time_final.split(":");
-              let data_time_final = new Date(data_final[2],data_final[1]-1,data_final[0],time_final[0],time_final[1]);
+        activityService.getActivitybyUid(uid).then((activity) => {
+                let data_start = event.dados.date_start.split("/");
+                let time_start = event.dados.time_start.split(":");
+                let data_time_start = new Date(data_start[2],data_start[1]-1,data_start[0],time_start[0],time_start[1]);
+                let data_final = event.dados.date_final.split("/");
+                let time_final = event.dados.time_final.split(":");
+                let data_time_final = new Date(data_final[2],data_final[1]-1,data_final[0],time_final[0],time_final[1]);
 
-              if(date >= data_time_start &&  date <= data_time_final){
-                    var activity_uid = activity.uid; // UID do doc no firestone
-                      var players = new Array();
-                      var tmp_players = activity.dados.players;
-                      var last = tmp_players.length;
-                      for(i=0;i<last;i++){
-                        if(tmp_players[i].user_UID == user_UID){
-                          score = tmp_players[i].score;
-                          alert('Retornando para o Jogo!');
-                          window.location.href = "./menu.html?activity_uid="+activity_uid;
-                        }else{
-                          let quiz_answered = new Array();
-                          let atual_quiz_answered = tmp_players[i].quiz_answered;
-                          for (j=0; i<atual_quiz_answered.length;j++){
-                            quiz_answered[j] = atual_quiz_answered[j];
+                if(date >= data_time_start &&  date <= data_time_final){
+                      var activity_uid = activity.uid; // UID do doc no firestone
+                        var players = new Array();
+                        var tmp_players = activity.dados.players;
+                        var last = tmp_players.length;
+                        for(i=0;i<last;i++){
+                          if(tmp_players[i].user_UID == user_UID){
+                            score = tmp_players[i].score;
+                            alert('Retornando para o Jogo!');
+                            window.location.href = "./menu.html?activity_uid="+activity_uid;
+                          }else{
+                            let quiz_answered = new Array();
+                            let atual_quiz_answered = tmp_players[i].quiz_answered;
+                            for (j=0; i<atual_quiz_answered.length;j++){
+                              quiz_answered[j] = atual_quiz_answered[j];
+                            }
+                            let tokens_quiz_used = new Array();
+                            let atual_tokens_quiz_used = tokens_quiz_used;
+                            for (j=0; i<atual_tokens_quiz_used.length;j++){
+                              tokens_quiz_used[j] = atual_tokens_quiz_used[j];
+                            }
+                            let user_UID = tmp_players[i].user_UID;
+                            let score = tmp_players[i].score;
+                            let ckeckin_date = tmp_players[i].ckeckin_date;
+                            let ckeckin_time = tmp_players[i].ckeckin_time;
+                            let timestamp = tmp_players[i].timestamp;
+                            players[i] = {user_UID,score,ckeckin_date,ckeckin_time, timestamp,quiz_answered,tokens_quiz_used};
                           }
-                          let tokens_quiz_used = new Array();
-                          let atual_tokens_quiz_used = tokens_quiz_used;
-                          for (j=0; i<atual_tokens_quiz_used.length;j++){
-                            tokens_quiz_used[j] = atual_tokens_quiz_used[j];
-                          }
-                          let user_UID = tmp_players[i].user_UID;
-                          let score = tmp_players[i].score;
-                          let ckeckin_date = tmp_players[i].ckeckin_date;
-                          let ckeckin_time = tmp_players[i].ckeckin_time;
-                          let timestamp = tmp_players[i].timestamp;
-                          players[i] = {user_UID,score,ckeckin_date,ckeckin_time, timestamp,quiz_answered,tokens_quiz_used};
                         }
-                      }
-                      let quiz_answered = [];
-                      let tokens_quiz_used = [];
-                      players[last] = {user_UID,score,ckeckin_date,ckeckin_time,timestamp,quiz_answered,tokens_quiz_used};
-                      activityService.update(activity_uid, {players}).then(window.location.href = "./menu.html?activity_uid="+activity_uid);
-              }else{
-                msg_error.innerHTML= "Atividade fora do prazo!";
-                alert_error.classList.add("show");
-                document.getElementById("bt-success").disabled = true;
-              }
+                        let quiz_answered = [];
+                        let tokens_quiz_used = [];
+                        players[last] = {user_UID,score,ckeckin_date,ckeckin_time,timestamp,quiz_answered,tokens_quiz_used};
+                        activityService.update(activity_uid, {players}).then(window.location.href = "./menu.html?activity_uid="+activity_uid);
+                }else{
+                  msg_error.innerHTML= "Atividade fora do prazo!";
+                  alert_error.classList.add("show");
+                  document.getElementById("bt-success").disabled = true;
+                }
+        })
       })
-    })
+    }
   }
 })
--- html/event/event-dashboard.js
+++ html/event/event-dashboard.js
@@ -1,32 +1,55 @@
 firebase.auth().onAuthStateChanged((User) => {
   if (User) {
-    let events_list = document.getElementById("events_list");
+    let active_events_list = document.getElementById("active_events_list");
+    let closed_events_list = document.getElementById("closed_events_list");
     eventService.getEventsByUserUID(User.uid).then((events) => {
-        let card_event = ``;
+        let card_active_event = ``;
+        let card_closed_event = ``;
         events.forEach(event => {
-            card_event = `<div class="card"><span class="event_dados" id="${event.uid}">${event.dados.name} - ${event.dados.state}</span>`;
+            let card_event = `<span class="event_dados" id="${event.uid}">${event.dados.name}</span>`;
             let players = event.dados.players;
             players.forEach(player => {
                 if(player.user_UID === User.uid){
-                  card_event =   card_event + 
-                    `<span id="coin" class="col-sm-3 ml-auto"><span class="badge rounded-pill bg-success"><span id="coins" class="badge bg-light text-dark">${player.coins}</span>&nbsp;AB@ COINS</span><br/></span>`;
+                  card_coins = 
+                  `<span id="coin" class="col-sm-3 ml-auto">`+
+                    `<span class="badge rounded-pill bg-success">`+
+                        `<span id="coins" class="badge bg-light text-dark">${player.coins}</span>`+
+                    `&nbsp;AB@ COINS`+
+                    `</span>`+
+                    `<br/>`+
+                  `</span>`;
                 }
             })
-            card_event =   card_event + `</div>`;
+            if (event.dados.state === "started"){
+              card_active_event = card_active_event +`<div class="card_active">${card_event}${card_coins}</div>`;
+            }
+            if (event.dados.state === "finished"){
+              card_closed_event = card_closed_event +`<div class="card_closed">${card_event}${card_coins}</div>`;
+            }
         })
-        events_list.innerHTML = card_event;
-
-        const card = events_list.querySelectorAll(".card");
-        // set onclick attribute to all available cards
-        for (i = 0; i < card.length; i++) {
-          card[i].setAttribute("onclick", "cardSelected(this)");
+        active_events_list.innerHTML = card_active_event;
+        closed_events_list.innerHTML = card_closed_event;
+        const card_active = events_list.querySelectorAll(".card_active");
+        const card_closed = events_list.querySelectorAll(".card_closed");
+        // set onclick attribute to all available cards active
+        for (i = 0; i < card_active.length; i++) {
+          card_active[i].setAttribute("onclick", "cardActiveSelected(this)");
         }
-
+         // set onclick attribute to all available cards closed
+        for (i = 0; i < card_closed.length; i++) {
+          card_closed[i].setAttribute("onclick", "cardClosedSelected(this)");
+        }
     });   
   } 
 });
   //if user clicked on card
-  function cardSelected(answer) {
-    let event_uid= answer.querySelector(".event_dados").id
-    window.location.href = "./activities-event-dashboard.html?event_uid="+event_uid;
+  function cardActiveSelected(answer) {
+    let event_uid= answer.querySelector(".event_dados").id;
+    window.location.href = `./activities-event-dashboard.html?event_uid="${event_uid}&state=started`;
+  }
+
+  //if user clicked on card
+  function cardClosedSelected(answer) {
+    let event_uid= answer.querySelector(".event_dados").id;
+    window.location.href = `./activities-event-dashboard.html?event_uid=${event_uid}&state=finished`;
   }
