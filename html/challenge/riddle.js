0c800448e9a6b1c659f94181f5408073f3d2aa4a
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-04-21 23:30:01 +0000

    Atualizar

--- html/challenge/riddle.js
+++ html/challenge/riddle.js
@@ -16,8 +16,15 @@ firebase.auth().onAuthStateChanged((User) => {
     const params = new URLSearchParams(window.location.search);
     var activity_id = params.get('activity_id'); 
     first_point = params.get('first_point');
-    let activity = getActivity(activity_id); 
+    let activity = getActivity(activity_id);
+    let level;
+    if(activity){
+      level = activity.level;
+    } 
     let points = getPoints(activity_id, user_UID);
+    if(points){
+      console.log(points);
+    }
     btn_voltar_tag.innerHTML = `<button class="badge bg-success p-2" onclick="voltar(${activity_id})" type="button">OK</button>`; 
     if(first_point){
       ground_control_point_id = params.get('ground_control_point_id'); //OK
@@ -26,13 +33,22 @@ firebase.auth().onAuthStateChanged((User) => {
       group_id = params.get('group_id');
       riddleService.getRiddleByGroundControlPointId(ground_control_point_next.trim(), group_id.trim()).then(riddles =>{
         showRiddle(riddles[0].dados);
-        setLogFirstQRCode(riddles[0].uid, activity_id, activity.level, points);   
+        validarValor(level)
+        console.log(level);
+        setLogFirstQRCode(riddles[0].uid, activity_id, activity.level, points)
       })
     }else{
       const riddle_id = params.get('riddle_id');
         riddleService.getRiddleByUID(riddle_id).then(riddle =>{
           showRiddle(riddle);
       })   
+    }
+
+    function validarValor(valor) {
+      if (valor === null) {
+        return false;
+      }
+      return true;
     }
 
   function showRiddle(riddle){
@@ -58,7 +74,6 @@ firebase.auth().onAuthStateChanged((User) => {
   }
 
   function setLogFirstQRCode(riddle_id, activity_id, level, points){
-      
       const time = (new Date()).toLocaleTimeString('pt-BR');
       const data = (new Date()).toLocaleDateString('pt-BR');
       let category = "challenge";
