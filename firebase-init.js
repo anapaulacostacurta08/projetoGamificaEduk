77e771ed90b931339a18c267a1dbb792c14e3677
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2025-04-23 15:19:05 -0300

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: $APIKEY,
-    authDomain: $AUTHDOMAIN,
-    projectId: $PROJECTID,
-    storageBucket: $STORAGEBUCKET,
-    messagingSenderId: $MESSAGINGSENDERID,
-    appId: $APPID,
-    measurementId: $MEASUREMENTID
+    apiKey: `${APIKEY}`,
+    authDomain: `{$AUTHDOMAIN}`,
+    projectId: `${PROJECTID}`,
+    storageBucket: `${STORAGEBUCKET}`,
+    messagingSenderId: `${MESSAGINGSENDERID}`,
+    appId: `${APPID}`,
+    measurementId: `${MEASUREMENTID}`
   };
 
 firebase.initializeApp(firebaseConfig);  
